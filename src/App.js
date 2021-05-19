a143ae2d9276e870f6c33cc88d6c04e3b7ec53b9
mahajkey <keyur.mahajan@tieto.com>
2021-05-20 04:42:54 +0530

    Added Particle React Js UI and Play Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
 import { StarPick } from './component/StarPick';
+import Particles from 'react-particles-js';
+import { useState } from 'react';
 
 function App() {
+  const [startGame, setStartGame] = useState(false)
   return (
-    <div className="App">
-      <StarPick />
+    <div className="App" style={{ backgroundColor: '#212121' }}>
+      <button onClick={() => setStartGame(true)}>Play Game</button>
+      {startGame === true ?
+        <div>
+          <StarPick />
+        </div> : null
+      }
+      <div style={{ backgroundColor: '#212121' }}>
+        <Particles id="particles-js"
+
+          height="95%"
+          width="95%"
+          style={{
+            position: 'relatice',
+
+          }}
+          params={{
+            particles: {
+
+              speed: {
+                value: 0.5
+              },
+              color: {
+                value: "#eceff1"
+              },
+              line_linked: {
+                color: {
+                  value: "#eceff1"
+                }
+              },
+              number: {
+                value: 40
+              },
+              size: {
+                value: 3
+              }
+            }
+          }}
+        />
+
+      </div>
+
+
+
+      <div style={{ backgroundColor: '#212121' }} >
+        <Particles id="particles-js"
+
+          height="95%"
+          width="95%"
+          style={{
+            position: 'relative',
+          }}
+          params={{
+            particles: {
+
+              speed: {
+                value: 0.5
+              },
+              color: {
+                value: "#eceff1"
+              },
+              line_linked: {
+                color: {
+                  value: "#eceff1"
+                }
+              },
+              number: {
+                value: 40
+              },
+              size: {
+                value: 3
+              }
+            }
+          }}
+        />
+      </div>
+      {/* 
+
+      <Particles id="particles-js"
+
+        height="95%"
+        width="95%"
+        style={{
+          position: 'absolute',
+          backgroundColor: 'blue'
+        }}
+        params={{
+          particles: {
+
+            speed: {
+              value: 0.5
+            },
+            color: {
+              value: "blue"
+            },
+            line_linked: {
+              color: {
+                value: "blue"
+              }
+            },
+            number: {
+              value: 80
+            },
+            size: {
+              value: 3
+            }
+          }
+        }}
+      /> */}
     </div>
   );
 }
